a8e606246fd4536c47285acff1b44298e75bc8b7
leejy23 <omega1227@naver.com>
2022-08-08 21:57:48 +0900

    No.41

---
+++ greeting.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src = "greeting.js"></script> 
+
+	<title></title>
+</head>
+<body>
+<script>
+	//외부에서 함수를 불러오니 가독성이 좋아짐
+	//모듈화가 되어서 어디서든welcome을 불러와 사용할 수 있음
+	alert (welcome());
+</script>
+</body>
+</html>
